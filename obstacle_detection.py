2196373d514cdb9e3ebc8e417fbcdc4b5b98a16a
jayson-poirier <jayson.poirier.1@ulaval.ca>
2025-10-30 11:23:53 -0400

    Convert range to coordinates in referance

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -6,36 +6,27 @@ from load_data import Map, Pose, Scan, ScanMetadata, load_map, load_poses, load_
 
 def obstacle_detection(map: Map, pose: Pose, scan: Scan):
     # TODO: Implement obstacle detection algorithm
-    MAX_DX = 0.5  # meters
-    MAX_DY = 0.5  # meters
+    MAX_RADIUS = 0.2
 
+    metadata = scan.metadata
 
     obstacles = [[]]
 
-    for i, scan_range in scan.ranges:
-        x, y = scan_range_to_coordinates(pose, scan_range, scan.metadata, i)
-        if len(obstacles[len(obstacles) - 1]) == 0:
-            obstacles.append([(x, y)])
-            continue
+    scan.ranges
 
-        last_x, last_y = obstacles[len(obstacles) - 1][-1]
-        dx = x - last_x
-        dy = y - last_y
+    range_in_robot_coordinates = [(
+                            distance * np.cos(metadata.angle_min + i * metadata.angle_increment),
+                            distance * np.sin(metadata.angle_min + i * metadata.angle_increment) 
+                            ) for i, distance in enumerate(scan.ranges)]
 
-        if dx > MAX_DX or dy > MAX_DY:
-            obstacles.append([(x, y)])
+    range_in_referance_plan = [(
+                            coords[0]*np.cos(pose.yaw) - coords[1]*np.sin(pose.yaw) + pose.x,
+                            coords[0]*np.sin(pose.yaw) + coords[1]*np.sin(pose.yaw) + pose.y
+                            ) for coords in range_in_robot_coordinates]
+
+
 
     return obstacles
-
-def scan_range_to_coordinates(pose: Pose, range: float, metadata: ScanMetadata, index: int) -> (float, float):
-    angle = metadata.angle_min + index * metadata.angle_increment
-    x_lidar = range * np.cos(angle)
-    y_lidar = range * np.sin(angle)
-
-    x = x_lidar*np.cos(pose.yaw) - y_lidar*np.sin(pose.yaw) + pose.x
-    y = x_lidar*np.sin(pose.yaw) + y_lidar*np.cos(pose.yaw) + pose.y
-
-    return x, y
 
 if __name__ == "__main__":
     from viz import draw_scene
