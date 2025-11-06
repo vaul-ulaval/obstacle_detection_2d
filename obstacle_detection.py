d15bfda51df138de16cc3979032abe3208d6a40e
plcas5 <plcas5@ulaval.ca>
2025-11-06 13:04:12 -0500

    refactor

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import numpy as np
-from matplotlib import pyplot as plt
-from load_data import Map, Pose, Scan, ScanMetadata, load_map, load_poses, load_scans
 from scipy.ndimage import binary_dilation
+from matplotlib import pyplot as plt
+from load_data import Map, Pose, Scan, load_map, load_poses, load_scans
 
 
 def obstacle_detection(map: Map, pose: Pose, scan: Scan):
@@ -10,17 +10,18 @@ def obstacle_detection(map: Map, pose: Pose, scan: Scan):
     MAX_RADIUS = 0.2
 
     ranges = np.array(scan.ranges, dtype=float)
-    angles = scan.metadata.angle_min + np.arange(ranges.size) * scan.metadata.angle_increment
+    angles = (
+        scan.metadata.angle_min + np.arange(ranges.size) * scan.metadata.angle_increment
+    )
 
-    x_r = ranges * np.cos(angles) # (N,)
-    y_r = ranges * np.sin(angles) # (N,)
-    robot_coords = np.stack((x_r, y_r), axis=1) # (N,2)
+    x_r = ranges * np.cos(angles)  # (N,)
+    y_r = ranges * np.sin(angles)  # (N,)
+    robot_coords = np.stack((x_r, y_r), axis=1)  # (N,2)
 
     cos_yaw = np.cos(pose.yaw)
     sin_yaw = np.sin(pose.yaw)
-    rotation = np.array([[cos_yaw, -sin_yaw],
-                         [sin_yaw, cos_yaw]]) # (2,2)
-    reference_coords = robot_coords @ rotation.T # (N,2)
+    rotation = np.array([[cos_yaw, -sin_yaw], [sin_yaw, cos_yaw]])  # (2,2)
+    reference_coords = robot_coords @ rotation.T  # (N,2)
     reference_coords += np.array([pose.x, pose.y])
 
     diffs = np.diff(reference_coords, axis=0)
@@ -28,26 +29,27 @@ def obstacle_detection(map: Map, pose: Pose, scan: Scan):
     breaks = np.where(r > MAX_RADIUS)[0] + 1
     splits = np.split(reference_coords, breaks)
     obstacles = [group.tolist() for group in splits if len(group) > 0]
-            
+
     obstacles = cluster_obstacles(obstacles)
 
     obstacles = remove_walls(obstacles, map)
 
     return obstacles
 
-def cluster_obstacles(obstacles, min_length = 0.5):
+
+def cluster_obstacles(obstacles, min_length=0.5):
     clustered_obstacles = []
 
     for i, obs in enumerate(obstacles):
         if len(obs) == 0:
             continue
-        coords_1 = np.array([obs[0], obs[(len(obs) - 1)//2], obs[-1]])
+        coords_1 = np.array([obs[0], obs[(len(obs) - 1) // 2], obs[-1]])
 
         for j, obs2 in enumerate(obstacles):
             if j <= i or len(obs2) == 0:
                 continue
 
-            coords_2 = np.array([obs2[0], obs2[len(obs2)//2], obs2[-1]])
+            coords_2 = np.array([obs2[0], obs2[len(obs2) // 2], obs2[-1]])
 
             diffs = coords_1[:, None, :] - coords_2[None, :, :]
 
@@ -59,6 +61,7 @@ def cluster_obstacles(obstacles, min_length = 0.5):
         clustered_obstacles.append(obs)
 
     return clustered_obstacles
+
 
 def world_to_map_indices(map: Map, x: float, y: float):
     col_f = (x - map.metadata.origin_x) / map.metadata.resolution
@@ -72,15 +75,23 @@ def world_to_map_indices(map: Map, x: float, y: float):
 
     return row, col
 
-def remove_walls(obstacles, map: Map, wall_thickness = 10):
 
-    walls = (map.grid == 100)
+def is_wall(coord, map: Map, thickened_map):
+    row, col = world_to_map_indices(map, coord[0], coord[1])
 
+    return thickened_map[row, col] == 100
+
+
+def get_thickened_map(map: Map, wall_thickness=10):
+    walls = map.grid == 100
     structure = np.ones((wall_thickness, wall_thickness))
-
     dilated = binary_dilation(walls, structure=structure)
 
-    thickened_map = np.where(dilated, 100, 0)
+    return np.where(dilated, 100, 0)
+
+
+def remove_walls(obstacles, map: Map):
+    thickened_map = get_thickened_map(map)
 
     for obs in obstacles:
         if len(obs) == 0:
@@ -88,13 +99,13 @@ def remove_walls(obstacles, map: Map, wall_thickness = 10):
 
         for coords in obs:
             x, y = coords[0], coords[1]
-            row, col = world_to_map_indices(map, x, y)
 
-            if thickened_map[row, col] == 100:
+            if is_wall((x, y), map, thickened_map):
                 obs.clear()
                 break
 
     return obstacles
+
 
 if __name__ == "__main__":
     from viz import draw_scene
@@ -114,4 +125,3 @@ if __name__ == "__main__":
     draw_scene(map, pose, scan, obstacles, ax)
 
     plt.show()
-    
